c0ef8974a4f406e8e5db437992c49db23923066f
flexxdarker <ksenzhakmax@gmail.com>
2023-09-14 20:19:11 +0300

    done hm14.08.2023

--- HM14.08.2023/HomeWork.cs
+++ HM14.08.2023/HomeWork.cs
@@ -13,49 +13,73 @@ namespace HM14._08._2023
         public void firstTask(int pages)
         {
             var a = library.Books.Where(x => x.Pages > pages);
-            foreach (var i in a) Console.WriteLine(i.Name);
+            foreach (var item in a)
+            {
+                Console.WriteLine(item.Name);
+            }
         }
 
         public void secondTask(char a)
         {
             var b = library.Books.Where(x => x.Name.StartsWith(a.ToString().ToLower()) || x.Name.StartsWith(a.ToString().ToUpper()));
-            foreach (var i in b) Console.WriteLine(i.Name);
+            foreach (var item in b)
+            {
+                Console.WriteLine(item.Name);
+            }
         }
 
         public void thirdTask(string name, string surname)
         {
             var a = library.Books.Where(x => x.Author.Name == name && x.Author.Surname == surname);
-            foreach (var i in a) Console.WriteLine(i.Name);
+            foreach (var item in a)
+            {
+                Console.WriteLine(item.Name);
+            }
         }
 
         public void fourhTask(string country)
         {
             var a = library.Authors.Where(x => x.Country.Name == country);
-            foreach (var i in a) Console.WriteLine(i.Name + " " + i.Surname);
+            foreach (var item in a)
+            {
+                Console.WriteLine(item.Name + " " + item.Surname);
+            }
         }
 
         public void fifthTask(int count)
         {
             var a = library.Books.Where(x => x.Name.Length < count);
-            foreach (var i in a) Console.WriteLine(i.Name + " " + i.Name.Length);
+            foreach (var item in a)
+            {
+                Console.WriteLine(item.Name + " " + item.Name.Length);
+            }
         }
 
         public void sixthTask(string country)
         {
             var a = library.Books.Where(x => x.Author.Country.Name == country).OrderByDescending(x => x.Pages);
-            foreach (var i in a) Console.WriteLine(i.Name + " " + i.Pages);
+            foreach (var item in a)
+            {
+                Console.WriteLine(item.Name + " " + item.Pages);
+            }
         }
 
         public void seventhTask(string country)
         {
             var a = library.Authors.Where(x => x.Country.Name == country).OrderByDescending(x => x.Books.Count);
-            foreach (var i in a) Console.WriteLine(i.Name + " " + i.Books.Count);
+            foreach (var item in a)
+            {
+                Console.WriteLine(item.Name + " " + item.Books.Count);
+            }
         }
 
         public void eightTask()
         {
             var a = library.Countries.OrderByDescending(x => x.Authors.Count);
-            foreach (var i in a) Console.WriteLine(i.Name + " " + i.Authors.Count);
+            foreach (var item in a)
+            {
+                Console.WriteLine(item.Name + " " + item.Authors.Count);
+            }
         }
     }
 }
